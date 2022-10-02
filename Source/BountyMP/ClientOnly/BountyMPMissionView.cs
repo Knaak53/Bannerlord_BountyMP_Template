fb2903cf46136d52e7d334df32c061d6de1ac05f
Manuel García <wtfknaak@gmail.com>
2022-10-02 03:44:06 +0200

    Delete trash code

--- Source/BountyMP/ClientOnly/BountyMPMissionView.cs
+++ Source/BountyMP/ClientOnly/BountyMPMissionView.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Text;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.MountAndBlade.View;
@@ -14,64 +13,32 @@ namespace BountyMP
         [ViewMethod("BountyMP")]
         public static MissionView[] OpenBountyMPMission(Mission mission)
         {
-            try
-            {
-                List<MissionView> missionViews = new List<MissionView>();
+            List<MissionView> missionViews = new List<MissionView>();
 
-                missionViews.Add(ViewCreator.CreateMissionServerStatusUIHandler());
-                missionViews.Add(ViewCreator.CreateMissionMultiplayerPreloadView(mission));
-                missionViews.Add(ViewCreator.CreateMultiplayerTeamSelectUIHandler());
-                missionViews.Add(ViewCreator.CreateMissionKillNotificationUIHandler());
-                missionViews.Add(ViewCreator.CreateMissionAgentStatusUIHandler(mission));
-                missionViews.Add(ViewCreator.CreateMissionMainAgentEquipmentController(mission));
-                missionViews.Add(ViewCreator.CreateMissionMainAgentCheerBarkControllerView(mission));
-                missionViews.Add(ViewCreator.CreateMissionMultiplayerEscapeMenu("BountyMP"));
-                missionViews.Add(ViewCreator.CreateMissionScoreBoardUIHandler(mission, false));
-                missionViews.Add(ViewCreator.CreateMultiplayerEndOfRoundUIHandler());
-                missionViews.Add(ViewCreator.CreateMultiplayerEndOfBattleUIHandler());
-                missionViews.Add(ViewCreator.CreateLobbyEquipmentUIHandler());
-                missionViews.Add(ViewCreator.CreateMissionAgentLabelUIHandler(mission));
-                missionViews.Add(ViewCreator.CreatePollProgressUIHandler());
-                missionViews.Add(ViewCreator.CreateMissionFlagMarkerUIHandler());
-                missionViews.Add(ViewCreator.CreateMultiplayerMissionDeathCardUIHandler());
-                missionViews.Add(ViewCreator.CreateOptionsUIHandler());
-                missionViews.Add(ViewCreator.CreateMissionMainAgentEquipDropView(mission));
-                missionViews.Add(ViewCreator.CreateMissionBoundaryCrossingView());
-                missionViews.Add(new MissionBoundaryWallView());
-                missionViews.Add(new MissionItemContourControllerView());
-                missionViews.Add(new MissionAgentContourControllerView());
-                missionViews.Add(new MissionGauntletBountyMPUI());
-                return missionViews.ToArray();
-            }
-            catch (Exception e) {
-
-                string fileName = @"C:\logs\log.txt";
-
-                    // Check if file already exists. If yes, delete it.     
-                    if (File.Exists(fileName))
-                    {
-                        File.Delete(fileName);
-                    }
-
-                    // Create a new file     
-                    using (FileStream fs = File.Create(fileName))
-                    {
-                        // Add some text to file    
-                        Byte[] title = new UTF8Encoding(true).GetBytes(e.StackTrace);
-                        fs.Write(title, 0, title.Length);
-                    }
-
-                    // Open the stream and read it back.    
-                    using (StreamReader sr = File.OpenText(fileName))
-                    {
-                        string s = "";
-                        while ((s = sr.ReadLine()) != null)
-                        {
-                            Console.Write(e.StackTrace);
-                        }
-                    }
-                return null;
-            }
+            missionViews.Add(ViewCreator.CreateMissionServerStatusUIHandler());
+            missionViews.Add(ViewCreator.CreateMissionMultiplayerPreloadView(mission));
+            missionViews.Add(ViewCreator.CreateMultiplayerTeamSelectUIHandler());
+            missionViews.Add(ViewCreator.CreateMissionKillNotificationUIHandler());
+            missionViews.Add(ViewCreator.CreateMissionAgentStatusUIHandler(mission));
+            missionViews.Add(ViewCreator.CreateMissionMainAgentEquipmentController(mission));
+            missionViews.Add(ViewCreator.CreateMissionMainAgentCheerBarkControllerView(mission));
+            missionViews.Add(ViewCreator.CreateMissionMultiplayerEscapeMenu("BountyMP"));
+            missionViews.Add(ViewCreator.CreateMissionScoreBoardUIHandler(mission, false));
+            missionViews.Add(ViewCreator.CreateMultiplayerEndOfRoundUIHandler());
+            missionViews.Add(ViewCreator.CreateMultiplayerEndOfBattleUIHandler());
+            missionViews.Add(ViewCreator.CreateLobbyEquipmentUIHandler());
+            missionViews.Add(ViewCreator.CreateMissionAgentLabelUIHandler(mission));
+            missionViews.Add(ViewCreator.CreatePollProgressUIHandler());
+            missionViews.Add(ViewCreator.CreateMissionFlagMarkerUIHandler());
+            missionViews.Add(ViewCreator.CreateMultiplayerMissionDeathCardUIHandler());
+            missionViews.Add(ViewCreator.CreateOptionsUIHandler());
+            missionViews.Add(ViewCreator.CreateMissionMainAgentEquipDropView(mission));
+            missionViews.Add(ViewCreator.CreateMissionBoundaryCrossingView());
+            missionViews.Add(new MissionBoundaryWallView());
+            missionViews.Add(new MissionItemContourControllerView());
+            missionViews.Add(new MissionAgentContourControllerView());
+            missionViews.Add(new MissionGauntletBountyMPUI());
+            return missionViews.ToArray();
         }
     }
 }
